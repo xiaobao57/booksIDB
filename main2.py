383fc4a413bed88d2318f2eedbd73fc87bf5ac3b
Dana Hsieh <dana_hsieh@utexas.edu>
2018-10-22 12:28:10 -0500

    rename main1.py to main2.py

---
+++ main2.py
@@ -0,0 +1,17 @@
+#-----------------------------------------
+# main2.py
+# creating first flask application
+#-----------------------------------------
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+ return render_template('hello.html')
+@app.route('/book/')
+def book():
+ return render_template('book.html')
+if __name__ == "__main__":
+ app.run()
+#----------------------------------------
+# end of main2.py
+#----------------------------------------- 
