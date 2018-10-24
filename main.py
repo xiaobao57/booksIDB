0f4a63dd1a6058f7ce4f7d373e37fb8faccadb41
Bryan Xiao <bryan.xiao@utexas.edu>
2018-10-24 12:07:02 -0500

    Made two folders for regular html links and Flask url_for links. Also created all model html pages and updated main.py with handlers for all html files

--- main.py
+++ main.py
@@ -12,14 +12,45 @@ app = Flask(__name__)
 def index():
  return render_template('hello.html')
 
-@app.route('/book/')
-def book():
- return render_template('book1.html')
+@app.route('/sorcerors-stone/')
+def sorcerors_stone():
+ return render_template('sorcerors-stone.html')
 
 
+#----------------------------------------
+# Authors
+#----------------------------------------
+
+@app.route('/jk-rowling/')
+def jk_rowling():
+ return render_template('jk-rowling.html')
+
+@app.route('/brandon-sanderson/')
+def brandon_sanderson():
+ return render_template('brandon-sanderson.html')
+
+@app.route('/carl-bernstein/')
+def carl_bernstein():
+ return render_template('carl-bernstein.html')
+
+#----------------------------------------
+# Publishers
+#----------------------------------------
+
+@app.route('/pottermore/')
+def pottermore():
+ return render_template('pottermore.html')
+
+@app.route('/simon-schuster/')
+def simon_schuster():
+ return render_template('simon-schuster.html')
+
+@app.route('/palgrave-mac/')
+def palgrave_mac():
+ return render_template('palgrave-mac.html')
 
 if __name__ == "__main__":
  app.run()
 #----------------------------------------
 # end of main.py
-#----------------------------------------- 
+#----------------------------------------
