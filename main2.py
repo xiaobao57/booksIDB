b8243442f8176f1188e0120947e1b9acbad33324
Dana Hsieh <dana_hsieh@utexas.edu>
2018-10-24 09:14:04 -0500

    added books1 to app

--- main2.py
+++ main2.py
@@ -3,13 +3,21 @@
 # creating first flask application
 #-----------------------------------------
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
  return render_template('hello.html')
+
 @app.route('/book/')
 def book():
- return render_template('book.html')
+ return render_template('book1.html')
+
+
+
 if __name__ == "__main__":
  app.run()
 #----------------------------------------
