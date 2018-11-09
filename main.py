bd9ada7ebda8e83c7099b839a5414ebd8c7705c6
Bryan Xiao <bryan.xiao@utexas.edu>
2018-11-08 18:39:39 -0600

    Reversed changes because of dependencies

--- main.py
+++ main.py
@@ -78,6 +78,63 @@ def publisherinfo(publisherid):
     publisher = db.session.query(Publisher).filter(Publisher.id == publisherid).first()
     return render_template('publisher.html', publisher = publisher)
  
+###Need to delete
+#----------------------------------------
+# Test Nav
+#----------------------------------------
+
+@app.route('/navtest')
+def navtest():
+    return render_template('navtest.html')
+ 
+#----------------------------------------
+# Books
+#----------------------------------------
+
+@app.route('/sorcerors-stone/')
+def sorcerors_stone():
+    return render_template('sorcerors-stone.html')
+
+@app.route('/mistborn/')
+def mistborn():
+    return render_template('mistborn.html')
+
+@app.route('/all-the-pres-men/')
+def all_the_pres_men():
+    return render_template('all-the-pres-men.html')
+
+
+#----------------------------------------
+# Authors
+#----------------------------------------
+
+@app.route('/jk-rowling/')
+def jk_rowling():
+ return render_template('jk-rowling.html')
+
+@app.route('/brandon-sanderson/')
+def brandon_sanderson():
+ return render_template('brandon-sanderson.html')
+
+@app.route('/carl-bernstein/')
+def carl_bernstein():
+ return render_template('carl-bernstein.html')
+
+#----------------------------------------
+# Publishers
+#----------------------------------------
+
+@app.route('/pottermore/')
+def pottermore():
+ return render_template('pottermore.html')
+
+@app.route('/simon-schuster/')
+def simon_schuster():
+ return render_template('simon-schuster.html')
+
+@app.route('/palgrave-mac/')
+def palgrave_mac():
+ return render_template('palgrave-mac.html')
 
 if __name__ == "__main__":
  app.run()
